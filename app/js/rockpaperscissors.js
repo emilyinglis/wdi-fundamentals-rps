7cc99a7fed42bf226b2ec59d2cded6fa116ac8c6
Emily Inglis <emilyinglis@Emilys-MacBook-Pro.local>
2015-09-08 11:44:24 +0100

    Update rps javascript with correct expressions x 2

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -21,19 +21,12 @@ function randomPlay() {
 /*           Write Your Code Below            */
 ////////////////////////////////////////////////
 
-function getPlayerMove(move) { 
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return move = move || getInput();
-
+function getPlayerMove(move) {
+    return 'move' || 'getInput()';
 }
 
 function getComputerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return move = move || randomPlay();
+    return 'move' || 'randomPlay()';
 }
 
 function getWinner(playerMove,computerMove) {
